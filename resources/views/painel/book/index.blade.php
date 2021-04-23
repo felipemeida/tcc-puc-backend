f0e8b4cd7bc3d1bb7dad98472f061c430755ccc1
Felipe Almeida <felipe@fourup.com.br>
2021-04-22 22:05:32 -0300

    corrigindo categoria

--- resources/views/painel/book/index.blade.php
+++ resources/views/painel/book/index.blade.php
@@ -29,7 +29,7 @@
                         <td>{{ $book['id'] }}</td>
                         <td>{{ $book['name'] }}</td>
                         <td>{{ $book['author'] }}</td>
-                        <td>{{ $book['category_id']  }}</td>
+                        <td>{{ $book->category->name  }}</td>
                         <td class="d-flex">
                             <a class="mr-2" href="{{ route('book.edit', $book['id']) }}">
                                 <button class="btn btn-warning">Editar</button>
